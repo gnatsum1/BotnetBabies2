5477491283b025b745050488ca68628ecd6d9a45
Alice <alicemilea04@gmail.com>
2025-07-12 17:47:14 -0400

    set up campaign structure

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import requests
 import os
 from dotenv import load_dotenv
+import pandas
 
 load_dotenv()  # load environment variables from .env file
-api_key = os.environ.get('API_KEY')
+api_key = os.environ.get('API_KEY', 'textbelt')
+
+
+"""Send a smishing campaign to a group of people defined in a csv file.
+
+filename: a csv file containing information in the format name,number,email
+"""
+def send_campaign(filename):
+    df = pandas.read_csv(filename)
+    print(df)
+    for index, row in df.iterrows():
+        person = row.to_dict()
+        send_text(person['number'], person['name'])
+        
 
 """Send a single text message using the Textbelt API.
 
@@ -18,8 +32,11 @@ def send_text(number, message):
     })
     print(resp.json())
 
+
 def main():
-    send_text(input(), 'hi hello')
+    filename = input("Please input a valid .csv filename: ")
+    send_campaign(filename)
+
 
 if __name__ == "__main__":
     main()
